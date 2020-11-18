ff20e646b3881bdf7ebd60905492e9b93953b246
SeungHyun Cho <c0305sh@naver.com>
2020-11-19 03:03:38 +0900

    Feature :: Pages - Blog 퍼블리싱 완료

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,65 +1,106 @@
 import React from 'react'
 import './blog.css'
 
+import post_thumb_1 from '../../styles/img/post_thumb_1.png'
+import profile from '../../styles/img/profile.jpg'
+import squre_plus from '../../styles/img/square_plus_icon_gray.png'
+import no_thumb_img from '../../styles/img/no_thumb_img.png'
+
 export default function index() {
     return (
         <div className="blog-container">
             <div className="blog-left-container">
                 <div className="post-container">
-                    <div className="post-left-container">이미지</div>
+                    <div className="post-left-container">
+                        <img src={post_thumb_1} />
+                    </div>
                     <div className="post-right-container">
-                        <div>게시글 제목</div>
-                        <div>세부내용</div>
+                        <div>React Hooks 란 무엇일까?</div>
+                        <div>React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들을 Side Effect 라고 한다. 화면 렌더링 반환 속도 ...</div>
                         <div>2020. 11.01 21.33</div>
                     </div>
                 </div>
                 <div className="post-container">
-                    <div className="post-left-container">이미지</div>
+                    <div className="post-left-container">
+                        <img src={post_thumb_1} />
+                    </div>
                     <div className="post-right-container">
-                        <div>게시글 제목</div>
-                        <div>세부내용</div>
+                        <div>React Hooks 란 무엇일까?</div>
+                        <div>React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들을 Side Effect 라고 한다. 화면 렌더링 반환 속도 ...</div>
                         <div>2020. 11.01 21.33</div>
                     </div>
                 </div>
                 <div className="post-container">
-                    <div className="post-left-container">이미지</div>
+                    <div className="post-left-container">
+                        <img src={post_thumb_1} />
+                    </div>
                     <div className="post-right-container">
-                        <div>게시글 제목</div>
-                        <div>세부내용</div>
+                        <div>React Hooks 란 무엇일까?</div>
+                        <div>React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들을 Side Effect 라고 한다. 화면 렌더링 반환 속도 ...</div>
                         <div>2020. 11.01 21.33</div>
                     </div>
                 </div>
                 <div className="post-container">
-                    <div className="post-left-container">이미지</div>
+                    <div className="post-left-container">
+                        <img src={post_thumb_1} />
+                    </div>
                     <div className="post-right-container">
-                        <div>게시글 제목</div>
-                        <div>세부내용</div>
+                        <div>React Hooks 란 무엇일까?</div>
+                        <div>React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들을 Side Effect 라고 한다. 화면 렌더링 반환 속도 ...</div>
                         <div>2020. 11.01 21.33</div>
                     </div>
                 </div>
                 <div className="post-container">
-                    <div className="post-left-container">이미지</div>
+                    <div className="post-left-container">
+                        <img src={post_thumb_1} />
+                    </div>
                     <div className="post-right-container">
-                        <div>게시글 제목</div>
-                        <div>세부내용</div>
+                        <div>React Hooks 란 무엇일까?</div>
+                        <div>React 컴포넌트가 화면에 렌더링된 이후에 비동기로 처리되어야 하는 부수적인 효과들을 Side Effect 라고 한다. 화면 렌더링 반환 속도 ...</div>
                         <div>2020. 11.01 21.33</div>
                     </div>
                 </div>
             </div>
             <div className="blog-right-container">
                 <div className="profile-container">
-                    <div>이미지</div>
+                    <div><img src={profile} /></div>
                     <div>SeungHyun Cho</div>
                     <div>luck is the residue of design</div>
                 </div>
                 <div className="search-container">
-                    검색 기능
+                    <input placeholder="검색기능을 이용해보세요!" />
                 </div>
                 <div className="category-container">
-                    카테고리 기능
+                    <div>카테고리</div>
+                    <div>All Categories<span className="category-post-count">(212)</span></div>
+                    <div className="category-main-container">
+                        <div>
+                            <img src={squre_plus} />
+                            React.js
+                            <span className="category-post-count">(11)</span>
+                        </div>
+                        <div>
+                            <img src={squre_plus} />
+                            ReactNavite.js
+                            <span className="category-post-count">(14)</span>
+                        </div>
+                        <div>
+                            <img src={squre_plus} />
+                            Laravel.js
+                            <span className="category-post-count">(52)</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="new-poster-container">
-                    새로운 게시글 기능
+                    <div>최신글</div>
+                    <div>
+                        <img src={no_thumb_img} />
+                            React Hooks 란 무엇일 ...
+                    </div>
+                    <div>
+                        <img src={no_thumb_img} />
+                            Github는 이것만 알면 다 ...
+                    </div>
                 </div>
             </div>
         </div>
